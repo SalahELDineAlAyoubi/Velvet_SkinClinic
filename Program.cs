5d9287aee618440049233f3b8f417264c3e4fa47
SalahELDineAlAyoubi <salah.a.al.ayoubi@gmail.com>
2026-02-08 19:28:56 +0200

    nettoyage

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//builder.Services.AddDbContext<GrandCompteContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("Grand_Compte"), sqlServerOptionsAction: sqlOptions =>
+//    {
+//        sqlOptions.CommandTimeout(60); // Set your desired timeout value in seconds
+//        sqlOptions.UseCompatibilityLevel(110);
+//    })
+//    .ConfigureWarnings(w => w.Ignore(SqlServerEventId.SavepointsDisabledBecauseOfMARS));
+//});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
